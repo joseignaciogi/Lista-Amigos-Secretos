e9ab4a4aa4b8ecbb12a008e5b8f3f1597fb62603
Jose Ignacio <ingunitec90@gmail.com>
2025-01-25 19:38:53 -0600

    app-js

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-let listUsuarios = [""];
+let listaAmigos = [];
 
+function agregarTexto(elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
 
 function agregarAmigo(){
-    
+    let newAmigo = document.getElementById('amigo').value;
+    console.log(newAmigo);
+    if(newAmigo === ""){
+        //alert("Por favor, inserte un nombre.");
+        agregarTexto('h2', '"Por favor, inserte un nombre."');
+    }else{
+        listaAmigos.push(newAmigo);
+    }
+         console.log(listaAmigos);
+         limpiarCaja();
+    return;
 }
+
+function limpiarCaja(){
+    document.getElementById('amigo').value = '';
+}
+
 
 function sortearAmigo(){
 
 }
-
